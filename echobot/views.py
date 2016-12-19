424c68e37c04d39199ebed5ab13de37576e607f9
hank <zebra840127@gmail.com>
2016-12-19 22:36:49 +0800

    make it better

--- echobot/views.py
+++ echobot/views.py
@@ -31,7 +31,8 @@ def callback(request):
                 if isinstance(event.message, TextMessage):
                     line_bot_api.reply_message(
                         event.reply_token,
-                        TextSendMessage(text=event.message.text)
+                        #TextSendMessage(text=event.message.text)
+                        TextSendMessage(text=event.res.text)
                     )
 
         return HttpResponse()
